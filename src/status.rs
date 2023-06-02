7cf60c225f00eb40d212cec673f09ac47d13d295
Nathan Flurry <git@nathanflurry.com>
2023-06-02 01:03:46 +0000

    Enable missing status fn

--- src/status.rs
+++ src/status.rs
@@ -5,17 +5,17 @@ pub fn info(msg: impl Display, data: impl Display) {
     eprintln!("{} {}", style(msg).bold().blue(), data);
 }
 
-// pub fn progress(msg: impl Display, data: impl Display) {
-// 	eprintln!("{} {}", style(msg).bold().green(), data);
-// }
+pub fn progress(msg: impl Display, data: impl Display) {
+	eprintln!("{} {}", style(msg).bold().green(), data);
+}
 
 pub fn success(msg: impl Display, data: impl Display) {
     eprintln!("{} {}", style(msg).bold().green(), data);
 }
 
-// pub fn warn(msg: impl Display, data: impl Display) {
-// 	eprintln!("{} {}", style(msg).bold().yellow(), data);
-// }
+pub fn warn(msg: impl Display, data: impl Display) {
+	eprintln!("{} {}", style(msg).bold().yellow(), data);
+}
 
 pub fn error(msg: impl Display, data: impl Display) {
     eprintln!("{} {}", style(msg).bold().red(), data);
