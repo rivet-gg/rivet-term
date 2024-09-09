d539a07d2920d47b88410f20e6d106b497cff1f5
MasterPtato <maxpopkov.contact@gmail.com>
2024-09-09 19:17:59 +0000

    fix: tabled

--- src/format.rs
+++ src/format.rs
@@ -5,10 +5,13 @@ pub fn table<T>(iter: impl IntoIterator<Item = T>)
 where
     T: Tabled,
 {
-    let mut table = Table::new(iter).with(tabled::Style::rounded());
+    let mut table = Table::new(iter);
+    table.with(tabled::settings::Style::rounded());
+    
     if let Some((w, _)) = term_size::dimensions() {
-        table = table.with(tabled::Width::wrap(w));
+        table.with(tabled::settings::Width::wrap(w));
     }
+    
     println!("{}", table);
 }
 
